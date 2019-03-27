9356ae0a7bf3e8e3561031647c4d43d7a4e0ba40
emmachlo <nassim.morouche1@gmail.com>
2019-03-27 15:46:46 +0100

    Sign up finished with errors conidtions

--- routes/signup.js
+++ routes/signup.js
@@ -6,6 +6,18 @@ const md5 = require('md5');
 const url = 'mongodb://localhost:27017/api-bdd';
 const dbName = 'notes-api';
 
+function isUsernameValid(str){
+    if(typeof(str)!== 'string'){
+        return false;
+    }
+    for(var i=0;i<str.length;i++){
+        if(str.charCodeAt(i)>122 || str.charCodeAt(i)<97){
+            return false;
+        }
+    }
+    return true;
+}
+
 /* GET notes JSON */
 router.get('/', async function(req, res) {
     const client = new MongoClient(url, { useNewUrlParser: true });
@@ -17,9 +29,8 @@ router.get('/', async function(req, res) {
 
         //Display all datas of the collection
         console.log('Displaying users\n');
-        let data = await col.find().toArray();
+        let data = await col.find({}).toArray();
         res.send(data);
-
     } catch (err) {
         res.send(err);
     }
@@ -34,20 +45,29 @@ router.post('/', async function(req, res) {
         const db = client.db(dbName);
         const col = db.collection('users');
         console.log('Connected\n');
+        let data = await col.find({}).toArray();
+        if(req.body.password.length <= 3) {
+            res.status(400).send({token: null, error: 'Le mot de passe doit contenir au moins 4 caractères'});
+        } else if(!isUsernameValid(req.body.username)) {
+            res.status(400).send({token: null, error: 'Votre identifiant ne doit contenir que des lettres minuscules non accentuées'});
+        } else if(req.body.username.length < 2 || req.body.username.length > 20) {
+            res.status(400).send({token: null, error: 'Votre identifiant doit contenir entre 2 et 20 caractères'});
+        }
+        else if (data.some(data => data.username === req.body.username)) {
+            res.status(400).send({token: null, error: 'Cet identifiant est déjà associé à un compte'});
+        } else {
+            //INSERT ONE DOCUMENT
+            await col.insertOne({
+                username: req.body.username,
+                password: md5(req.body.password)
+            });
+            res.send({token: 'token', error: null});
+        }
 
-        //INSERT ONE DOCUMENT
-        let username = req.body.username;
-        let password = md5(req.body.password);
-        await col.insertOne({
-            username: username,
-            password: password
-        });
-        res.send('token');
-/*
       //DELETING ONE DOCUMENT
         console.log('Deleting One element');
-        col.deleteMany({username: 'nassim2'});
-*/
+        col.deleteMany({username: 'test'});
+
     } catch (err) {
         res.send(err);
     }
